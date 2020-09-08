ea4b8f966d809b7e17aeb5d90f5a8100242e7f98
serenus <dev@digitalsnuff.com>
2020-09-08 19:33:06 +0200

    Strapi

---
+++ strapi-app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '62ad2fd52b98bc789bc44f22950bae16'),
+    },
+  },
+});
---
+++ strapi-app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a69f2090-3eee-46d4-a4ae-e1aa755a2582'
+};
